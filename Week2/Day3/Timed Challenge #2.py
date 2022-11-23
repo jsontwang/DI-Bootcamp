3d289d941bd8081c3dc5b5f333c1f9ca4f1d3ae9
jsontwang <118206053+jsontwang@users.noreply.github.com>
2022-11-23 21:59:20 +0400

    Create Timed Challenge #2.py

---
+++ Week2/Day3/Timed Challenge #2.py
@@ -0,0 +1,15 @@
+# Reverse The Sentence
+# Write a program to reverse the sentence wordwise.
+
+# Input:
+# You have entered a wrong domain
+# Output:
+# domain wrong a entered have You
+
+string1 = "You have entered a wrong domain"
+str1 = string1.split(" ")
+
+
+str1[::-1]
+" ".join(str1[::-1])
+
